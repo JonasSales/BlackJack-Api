c236e7ef2a3e357f16494cd2a19644cfa3fd495b
Jonas <jonassales510@gmail.com>
2025-02-23 12:27:48 -0300

    Atualização do BlackjackController para comportar n mesas simultaneamente.

--- src/main/java/com/example/demo/blackjack/api/controller/BlackjackController.java
+++ src/main/java/com/example/demo/blackjack/api/controller/BlackjackController.java
@@ -1,162 +1,199 @@
-package com.example.demo.blackjack.api.controller;
+    package com.example.demo.blackjack.api.controller;
 
-import com.example.demo.auth.dto.UserDTO;
-import com.example.demo.auth.service.UserService;
-import com.example.demo.blackjack.api.DTO.playerRequest;
-import com.example.demo.blackjack.model.Card;
-import com.example.demo.blackjack.model.Player;
-import com.example.demo.blackjack.domain.service.BlackjackGameService;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+    import com.example.demo.auth.dto.UserDTO;
+    import com.example.demo.auth.service.AuthenticationService;
+    import com.example.demo.auth.service.UserService;
+    import com.example.demo.blackjack.api.DTO.playerRequest;
+    import com.example.demo.blackjack.model.Card;
+    import com.example.demo.blackjack.model.Player;
+    import com.example.demo.blackjack.domain.service.BlackjackGameService;
+    import com.example.demo.blackjack.model.Table;
+    import jakarta.servlet.http.HttpServletRequest;
+    import jakarta.servlet.http.HttpServletResponse;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
 
+    import java.util.HashMap;
+    import java.util.List;
+    import java.util.Map;
+    import java.util.UUID;
+    import java.util.stream.Collectors;
 
+    @RestController
+    @RequestMapping("/blackjack")
+    @CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+    public class BlackjackController {
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+        private final BlackjackGameService gameFunctions;
+        private final UserService userService;
+        private final AuthenticationService authenticationService;
+        private final Map<UUID, Table> mesas = new HashMap<>(); // Array de mesas
 
-@RestController
-@RequestMapping("/blackjack")
-@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
-public class BlackjackController {
-
-    private final BlackjackGameService gameFunctions;
-    private final UserService userService;
-
-    public BlackjackController(BlackjackGameService gameFunctions, UserService userService) {
-        this.gameFunctions = gameFunctions;
-        this.userService = userService;
-    }
-
-    // Listar jogadores
-    @GetMapping("/jogadores")
-    public ResponseEntity<List<Player>> jogadores() {
-        List<Player> jogadores = gameFunctions.getJogadores();
-        return new ResponseEntity<>(jogadores, HttpStatus.OK);
-    }
-
-    // Jogador Atual
-    @GetMapping("/jogadoratual")
-    public ResponseEntity<List<Player>> jogadoresAtuais() {
-        List<Player> players = gameFunctions.getJogadores();
-        List<Player> jogadoresAtuais = players.stream()
-                .filter(player -> player.isJogadorAtual() && !player.isPerdeuTurno() && !player.isStand())
-                .collect(Collectors.toList());
-        return new ResponseEntity<>(jogadoresAtuais, HttpStatus.OK);
-    }
-
-    // Adicionar jogador
-    @PostMapping("/adicionar")
-    public ResponseEntity<Map<String, String>> adicionarJogador(HttpServletRequest request) {
-        UserDTO userDTO = userService.getUserFromToken(request);
-        Player jogador = new Player(userDTO);
-        Map<String, String> response = new HashMap<>();
-        boolean sucesso = gameFunctions.adicionarJogador(jogador);
-        response.put("message", sucesso ? "Jogador " + jogador.getUser().getName() +
-                " adicionado à mesa!" : "Não foi possível adicionar " + jogador.getUser().getName() + ".");
-        return new ResponseEntity<>(response, sucesso ? HttpStatus.OK : HttpStatus.BAD_REQUEST);
-    }
-
-    // Iniciar jogo
-    @PostMapping("/iniciar")
-    public ResponseEntity<Map<String, String>> iniciarJogo() {
-        Map<String, String> response = new HashMap<>();
-        if (gameFunctions.getJogadores().size() < 2) {
-            response.put("message", "É necessário pelo menos dois jogadores para iniciar o jogo!");
-            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        public BlackjackController(BlackjackGameService gameFunctions, UserService userService, AuthenticationService authenticationService) {
+            this.gameFunctions = gameFunctions;
+            this.userService = userService;
+            this.authenticationService = authenticationService;
         }
-        gameFunctions.iniciarJogo();
-        response.put("message", "Jogo iniciado! Cartas distribuídas!");
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
 
-    // Cartas
-    @GetMapping("/cartas")
-    public ResponseEntity<Map<String, List<String>>> getCartasDeJogadores() {
-        List<Player> players = gameFunctions.getJogadores();
-        if (players.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        // Listar jogadores de uma mesa
+        @GetMapping("/mesas/{mesaId}/jogadores")
+        public ResponseEntity<List<Player>> jogadores(@PathVariable UUID mesaId) {
+            Table mesa = gameFunctions.getMesas().get(mesaId);
+            if (mesa == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+            List<Player> jogadores =  mesa.getJogadores();
+            return new ResponseEntity<>(jogadores, HttpStatus.OK);
         }
-        Map<String, List<String>> cartasPorJogador = players.stream()
-                .collect(Collectors.toMap(
-                        player -> player.getUser().getName(), // Obtendo o nome do usuário dentro do Player
-                        player -> player.getMao().stream()
-                                .map(Card::toString)
-                                .collect(Collectors.toList())
-                ));
-        return new ResponseEntity<>(cartasPorJogador, HttpStatus.OK);
-    }
 
-
-    // Finalizar jogo
-    @GetMapping("/finalizar")
-    public ResponseEntity<Map<String, String>> finalizarJogo() {
-        Map<String, String> response = new HashMap<>();
-        String fimDeJogo = gameFunctions.finalizarJogo();
-        gameFunctions.resetarJogo();
-        response.put("message", fimDeJogo);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    // Proximo Jogador
-    @PostMapping("/proximoJogador")
-    public Player proximoJogador() {
-        return gameFunctions.proximoJogador();
-    }
-
-    // Jogada
-    @PostMapping("/jogada")
-    public ResponseEntity<String> realizarJogada(HttpServletRequest request, @RequestBody playerRequest jogada) {
-        // Recupera o usuário a partir do token da requisição
-        UserDTO userDTO = userService.getUserFromToken(request);
-        Player jogador = new Player(userDTO);
-        // Executa a jogada e valida se é uma jogada válida
-        boolean jogadaValida = gameFunctions.jogada(jogador, jogada.getJogada());
-        if (!jogadaValida) {
-            String errorMessage = "Jogada inválida para " + jogador.getUser().getName();
-            return ResponseEntity.badRequest().body(errorMessage);
+        // Jogador Atual de uma mesa
+        @GetMapping("/mesas/{mesaId}/jogadoratual")
+        public ResponseEntity<List<Player>> jogadoresAtuais(@PathVariable UUID mesaId) {
+            Table mesa = mesas.get(mesaId);
+            if (mesa == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+            List<Player> jogadoresAtuais = mesa.getJogadores().stream()
+                    .filter(player -> player.isJogadorAtual() && !player.isPerdeuTurno() && !player.isStand())
+                    .collect(Collectors.toList());
+            return new ResponseEntity<>(jogadoresAtuais, HttpStatus.OK);
         }
-        // Verifica se todos os jogadores encerraram a mão
-        if (gameFunctions.verificarTodosEncerraram()) {
-            gameFunctions.finalizarJogo();
-            String finalMessage = "Todos os jogadores encerraram a mão. O jogo foi finalizado.";
-            return ResponseEntity.ok(finalMessage);
+
+        // Adicionar jogador à mesa
+        @PostMapping("/mesas/{mesaId}/adicionar")
+        public ResponseEntity<Map<String, String>> adicionarJogador(@PathVariable UUID mesaId, HttpServletRequest request) {
+            UserDTO userDTO = userService.getUserFromToken(request);
+            Player jogador = new Player(userDTO);
+            Map<String, String> response = new HashMap<>();
+            Table mesa = mesas.get(mesaId);
+            if (mesa == null) {
+                response.put("message", "Mesa não encontrada");
+                return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+            }
+            boolean sucesso = mesa.adicionarJogador(jogador);
+            response.put("message", sucesso ? "Jogador " + jogador.getUser().getName() +
+                    " adicionado à mesa!" : "Não foi possível adicionar " + jogador.getUser().getName() + ".");
+            return new ResponseEntity<>(response, sucesso ? HttpStatus.OK : HttpStatus.BAD_REQUEST);
         }
-        // Obtém o próximo jogador válido
-        Player proximoJogador = gameFunctions.proximoJogador();
-        if (proximoJogador == null) {
-            gameFunctions.finalizarJogo();
-            String finalMessage = "Não há jogadores válidos restantes. O jogo foi finalizado.";
-            return ResponseEntity.ok(finalMessage);
+
+        // Iniciar jogo na mesa
+        @PostMapping("/mesas/{mesaId}/iniciar")
+        public ResponseEntity<Map<String, String>> iniciarJogo(@PathVariable UUID mesaId) {
+            Map<String, String> response = new HashMap<>();
+            Table mesa = mesas.get(mesaId);
+            if (mesa == null) {
+                response.put("message", "Mesa não encontrada");
+                return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+            }
+            if (mesa.getJogadores().size() < 2) {
+                response.put("message", "É necessário pelo menos dois jogadores para iniciar o jogo!");
+                return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+            }
+            mesa.iniciarJogo();
+            response.put("message", "Jogo iniciado! Cartas distribuídas!");
+            return new ResponseEntity<>(response, HttpStatus.OK);
         }
-        String nextMessage = "Próximo jogador: " + proximoJogador.getUser().getName();
-        return ResponseEntity.ok(nextMessage);
+
+        // Cartas de uma mesa
+        @GetMapping("/mesas/{mesaId}/cartas")
+        public ResponseEntity<Map<String, List<String>>> getCartasDeJogadores(@PathVariable UUID mesaId) {
+            Table mesa = mesas.get(mesaId);
+            if (mesa == null) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            Map<String, List<String>> cartasPorJogador = mesa.getJogadores().stream()
+                    .collect(Collectors.toMap(
+                            player -> player.getUser().getName(),
+                            player -> player.getMao().stream()
+                                    .map(Card::toString)
+                                    .collect(Collectors.toList())
+                    ));
+            return new ResponseEntity<>(cartasPorJogador, HttpStatus.OK);
+        }
+
+        // Jogada na mesa
+        @PostMapping("/mesas/{mesaId}/jogada")
+        public ResponseEntity<String> realizarJogada(@PathVariable UUID mesaId, HttpServletRequest request, @RequestBody playerRequest jogada) {
+            UserDTO userDTO = userService.getUserFromToken(request);
+            Player jogador = new Player(userDTO);
+
+            boolean jogadaValida = gameFunctions.jogada(jogador, jogada.getJogada(), mesaId);
+            if (!jogadaValida) {
+                String errorMessage = "Jogada inválida para " + jogador.getUser().getName();
+                return ResponseEntity.badRequest().body(errorMessage);
+            }
+            if (gameFunctions.verificarTodosEncerraram(mesaId)) {
+                gameFunctions.finalizarJogo(mesaId);
+                String finalMessage = "Todos os jogadores encerraram a mão. O jogo foi finalizado.";
+                return ResponseEntity.ok(finalMessage);
+            }
+            Player proximoJogador = gameFunctions.proximoJogador(mesaId);
+            if (proximoJogador == null) {
+                gameFunctions.finalizarJogo(mesaId);
+                String finalMessage = "Não há jogadores válidos restantes. O jogo foi finalizado.";
+                return ResponseEntity.ok(finalMessage);
+            }
+            String nextMessage = "Próximo jogador: " + proximoJogador.getUser().getName();
+            return ResponseEntity.ok(nextMessage);
+        }
+
+        @GetMapping("/partidaIniciada/{mesaId}")
+        public ResponseEntity<Map<String, String>> partidaIniciada(@PathVariable UUID mesaId) {
+            Map<String, String> response = new HashMap<>();
+
+            Table mesa = gameFunctions.encontrarMesaPorId(mesaId);
+            if (mesa == null) {
+                response.put("jogoIniciado", "false");
+                return ResponseEntity.ok(response);
+            }
+            response.put("jogoIniciado", String.valueOf(mesa.getJogoIniciado()));
+            return ResponseEntity.ok(response);
+        }
+
+        @PostMapping("/criarMesa")
+        public ResponseEntity<String> criarMesa(HttpServletResponse response) {
+            Table mesa = gameFunctions.criarNovaMesa();
+            mesas.put(mesa.getId(), mesa);
+            String mesaToken = authenticationService.gerarTokenMesa(mesa.getId(), response);
+            return ResponseEntity.ok(mesaToken);
+        }
+
+
+        @GetMapping("/mesa/{mesaId}")
+        public ResponseEntity<?> acessarMesa(@PathVariable UUID mesaId, @RequestHeader("Authorization") String token) {
+            UUID mesaIdFromToken = authenticationService.validarTokenMesa(token);
+            if (mesaIdFromToken == null || !mesaIdFromToken.equals(mesaId)) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Token inválido ou expirado"));
+            }
+
+            Table mesa = mesas.get(mesaId);
+            if (mesa == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "Mesa não encontrada"));
+            }
+
+            return ResponseEntity.ok(Map.of(
+                    "mesaId", mesa.getId(),
+                    "jogoIniciado", mesa.getJogoIniciado(),
+                    "jogadores", mesa.getJogadores()
+            ));
+        }
+
+        @GetMapping("/mesas")
+        public ResponseEntity<List<Map<String, Object>>> listarMesas() {
+            List<Map<String, Object>> mesasComSub = mesas.values().stream()
+                    .map(mesa -> {
+                        Map<String, Object> mesaInfo = new HashMap<>();
+                        mesaInfo.put("mesaId", mesa.getId());// Acredito que o "sub" se refira ao criador da mesa
+                        return mesaInfo;
+                    })
+                    .collect(Collectors.toList());
+
+            if (mesasComSub.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(mesasComSub);
+            }
+
+            return ResponseEntity.ok(mesasComSub);
+        }
+
     }
-
-    @GetMapping("partidaIniciada")
-    public ResponseEntity<Map<String, String>> partidaIniciada() {
-        Map<String, String> response = new HashMap<>();
-        boolean jogoIniciado = gameFunctions.getMesa().isJogoIniciado();
-        // Se o jogo foi iniciado, responda com "true", senão "false"
-        response.put("jogoIniciado", String.valueOf(jogoIniciado));
-        return ResponseEntity.ok(response);
-    }
-
-    /*@PostMapping("/criarMesa")
-    public ResponseEntity<Map<String, String>> criarMesa() {
-        Long mesaId = gameFunctions.criarNovaMesa(); // Criar a mesa e obter ID
-
-        // Gera o token JWT para a mesa
-        String mesaToken = AuthenticationService.generateGameToken(mesaId);
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Mesa criada com sucesso!");
-        response.put("mesaToken", mesaToken);
-        return ResponseEntity.ok(response);
-    }*/
-
-
-}
-
