7138f7579337340126dbc5e8b9272436eb2b1bc2
Jonas <jonassales510@gmail.com>
2025-03-15 13:44:23 -0300

    Ajustes no código de ordenação, futuramente ordenar de forma reversa.

--- src/main/java/com/example/demo/blackjack/utils/Ordenacao.java
+++ src/main/java/com/example/demo/blackjack/utils/Ordenacao.java
@@ -6,64 +6,71 @@ import java.util.List;
 
 public class Ordenacao{
 
+    public static void ordenarPorMoney(List<UserDTO> vetor, int inicio, int fim) {
+        if (inicio < fim) {
+            int i = inicio;
+            int j = fim;
+            UserDTO pivot = vetor.get((inicio + fim) / 2);
 
-
-    public static List<UserDTO> ordenarPorMoney(List<UserDTO> vetor, int inicio, int fim){
-        int i = inicio;
-        int j = fim;
-        UserDTO pivot = vetor.get((inicio + fim) / 2);
-
-        while(i < j){
-            while (vetor.get(i).getMoney() < pivot.getMoney()){
-                i += 1;
+            while (i <= j) {
+                // Avança i enquanto o elemento for menor que o pivot
+                while (vetor.get(i).getMoney() < pivot.getMoney()) {
+                    i++;
+                }
+                // Recua j enquanto o elemento for maior que o pivot
+                while (vetor.get(j).getMoney() > pivot.getMoney()) {
+                    j--;
+                }
+                // Troca os elementos se i <= j
+                if (i <= j) {
+                    UserDTO aux = vetor.get(i);
+                    vetor.set(i, vetor.get(j));
+                    vetor.set(j, aux);
+                    i++;
+                    j--;
+                }
             }
-            while (vetor.get(j).getMoney() > pivot.getMoney()){
-                j -= 1;
+
+            // Recursão para as sublistas
+            if (inicio < j) {
+                ordenarPorMoney(vetor, inicio, j);
             }
-            if (i < j){
-                UserDTO aux = vetor.get(i);
-                vetor.set(i, vetor.get(j));
-                vetor.set(j, aux);
-                i +=1;
-                j -=1;
+            if (i < fim) {
+                ordenarPorMoney(vetor, i, fim);
             }
         }
-        if (j > inicio){
-            ordenarPorMoney(vetor, inicio, j);
-        }
-        else if (i < fim){
-            ordenarPorMoney(vetor, i, fim);
-        }
-        return vetor;
     }
 
 
-    public static List<UserDTO> ordenarPorVitoria(List<UserDTO> vetor, int inicio, int fim){
-        int i = inicio;
-        int j = fim;
-        UserDTO pivot = vetor.get((inicio + fim) / 2);
+    public static void ordenarPorVitoria(List<UserDTO> vetor, int inicio, int fim) {
+        if (inicio < fim) {
+            int i = inicio;
+            int j = fim;
+            UserDTO pivot = vetor.get((inicio + fim) / 2);
 
-        while(i < j){
-            while (vetor.get(i).getPartidasGanhas() < pivot.getPartidasGanhas()){
-                i += 1;
+            while (i <= j) {
+                while (vetor.get(i).getPartidasGanhas() < pivot.getPartidasGanhas()) {
+                    i++;
+                }
+                while (vetor.get(j).getPartidasGanhas() > pivot.getPartidasGanhas()) {
+                    j--;
+                }
+
+                if (i <= j) {
+                    UserDTO aux = vetor.get(i);
+                    vetor.set(i, vetor.get(j));
+                    vetor.set(j, aux);
+                    i++;
+                    j--;
+                }
             }
-            while (vetor.get(j).getPartidasGanhas() > pivot.getPartidasGanhas()){
-                j -= 1;
+
+            if (inicio < j) {
+                ordenarPorVitoria(vetor, inicio, j);
             }
-            if (i < j){
-                UserDTO aux = vetor.get(i);
-                vetor.set(i, vetor.get(j));
-                vetor.set(j, aux);
-                i +=1;
-                j -=1;
+            if (i < fim) {
+                ordenarPorVitoria(vetor, i, fim);
             }
         }
-        if (j > inicio){
-            ordenarPorVitoria(vetor, inicio, j);
-        }
-        else if (i < fim){
-            ordenarPorVitoria(vetor, i, fim);
-        }
-        return vetor;
     }
 }
